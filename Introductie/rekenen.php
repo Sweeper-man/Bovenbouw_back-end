739913e3b6080ed921e8fd8d92472f09a2ae43ba
Seantheman01 <sean.v.g@outlook.com>
2024-05-16 22:13:54 +0200

    part 1

--- Introductie/rekenen.php
+++ Introductie/rekenen.php
@@ -1,5 +1,9 @@
 <?php
-for ($x = 0; $x <= 10; $x++) {
-    echo $x;
-}
+$getal1 = rand(1,10);
+$getal2 = rand(1,10);
+
+echo ($getal1 . " + " . $getal2 . " = " . $getal1 + $getal2 . "<br>");
+echo ($getal1 . " - " . $getal2 . " = " . $getal1 - $getal2 . "<br>");
+echo ($getal1 . " × " . $getal2 . " = " . $getal1 * $getal2 . "<br>");
+echo ($getal1 . " : " . $getal2 . " = " . $getal1 / $getal2 . "<br>");
 ?>
