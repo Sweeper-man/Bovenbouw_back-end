c40f183e2b3fc9f61162c8e88b537ecb1929f876
Seantheman01 <sean.v.g@outlook.com>
2024-05-20 21:33:07 +0200

    fixed step 2

--- Introductie/rekenen.php
+++ Introductie/rekenen.php
@@ -1,4 +1,5 @@
 <?php
+// A
 $getal1 = rand(1,10);
 $getal2 = rand(1,10);
 
@@ -8,15 +9,28 @@ echo ($getal1 . " × " . $getal2 . " = " . $getal1 * $getal2 . "<br>");
 echo ($getal1 . " : " . $getal2 . " = " . $getal1 / $getal2 . "<br>");
 echo "<br>";
 
+// B
+// #1
 for ($x = 0; $x <= 10; $x++) {
     echo "6 × ".$x." = ". 6*$x."<br>";
 }
 echo "<br>";
 
+// #2
+function numbers($n) {
+    for ($y = 0; $y <= $n; $y++) {
+        echo $y . "<br>";
+    }
+}
+
+numbers(6);
+echo "<br>";
+
+// #3
 $getallen = array("3", "5", "8", "12");
 foreach ($getallen as $getal) {
-    for ($y = 0; $y <= 10; $y++) {
-        echo $getal." × ".$y." = ".$getal*$y."<br>";
+    for ($i = 0; $i <= 10; $i++) {
+        echo $getal." × ".$i." = ".$getal*$i."<br>";
     }
     echo "<br>";
 }
