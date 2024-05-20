5f6696db2e7d707b4cd3042dec587208e49ff586
Seantheman01 <sean.v.g@outlook.com>
2024-05-20 21:20:39 +0200

    added other 3 steps

--- Introductie/rekenen.php
+++ Introductie/rekenen.php
@@ -6,6 +6,18 @@ echo ($getal1 . " + " . $getal2 . " = " . $getal1 + $getal2 . "<br>");
 echo ($getal1 . " - " . $getal2 . " = " . $getal1 - $getal2 . "<br>");
 echo ($getal1 . " × " . $getal2 . " = " . $getal1 * $getal2 . "<br>");
 echo ($getal1 . " : " . $getal2 . " = " . $getal1 / $getal2 . "<br>");
+echo "<br>";
 
+for ($x = 0; $x <= 10; $x++) {
+    echo "6 × ".$x." = ". 6*$x."<br>";
+}
+echo "<br>";
 
+$getallen = array("3", "5", "8", "12");
+foreach ($getallen as $getal) {
+    for ($y = 0; $y <= 10; $y++) {
+        echo $getal." × ".$y." = ".$getal*$y."<br>";
+    }
+    echo "<br>";
+}
 ?>
