19ccd85a31359f775f511c85e02876880ed09e13
Seantheman01 <sean.v.g@outlook.com>
2024-05-22 09:07:24 +0200

    fixes

--- Introductie/goedemorgen/goedemorgen.php
+++ Introductie/goedemorgen/goedemorgen.php
@@ -21,12 +21,12 @@ $tijd = date("H:i:s");
 
 if ($tijd >= "06:00:00" && $tijd < "12:00:00") {
     $achtergrond = '<body style="background-image: url(images/morning.png)">';
-    echo "<h1>Goedemorgen!</h1>";
+    echo "<h1>Goede morgen!</h1>";
 }
 
 else if ($tijd >= "12:00:00" && $tijd < "18:00:00") {
     $achtergrond = '<body style="background-image: url(images/afternoon.png)">';
-    echo "<h1>Goedemiddag!</h1>";
+    echo "<h1>Goede middag!</h1>";
 }
 
 else if ($tijd >= "18:00:00" && $tijd < "24:00:00") {
