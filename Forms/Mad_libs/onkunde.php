7d256cb9d487372471a02b6dff14a3e28b2abf9b
Seantheman01 <sean.v.g@outlook.com>
2024-06-24 11:51:39 +0200

    file name fixed

---
+++ Forms/Mad_libs/onkunde.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onkunde</title>
+</head>
+<body>
+    <form>
+        <label for="eerste">Wat zou je graag willen kunnen?</label>
+        <input type="text" id="eerste" name="eerste" required><br>
+        <label for="tweede">Met welk persoon kun je goed opschieten?</label>
+        <input type="text" id="tweede" name="tweede" required><br>
+        <label for="derde">Wat is je favoriete getal?</label>
+        <input type="text" id="derde" name="derde" required><br>
+        
+    </form>
+</body>
+</html>
