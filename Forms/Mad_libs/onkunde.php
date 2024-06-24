10743bdeb9759f378342c5e21e44398de5e2af52
Seantheman01 <sean.v.g@outlook.com>
2024-06-24 11:59:52 +0200

    all form questions added on onkunde

--- Forms/Mad_libs/onkunde.php
+++ Forms/Mad_libs/onkunde.php
@@ -7,13 +7,28 @@
 </head>
 <body>
     <form>
-        <label for="eerste">Wat zou je graag willen kunnen?</label>
-        <input type="text" id="eerste" name="eerste" required><br>
-        <label for="tweede">Met welk persoon kun je goed opschieten?</label>
-        <input type="text" id="tweede" name="tweede" required><br>
-        <label for="derde">Wat is je favoriete getal?</label>
-        <input type="text" id="derde" name="derde" required><br>
-        
+        <label for="één">Wat zou je graag willen kunnen?</label>
+        <input type="text" id="één" name="één" required><br>
+
+        <label for="twee">Met welk persoon kun je goed opschieten?</label>
+        <input type="text" id="twee" name="twee" required><br>
+
+        <label for="drie">Wat is je favoriete getal?</label>
+        <input type="text" id="drie" name="drie" required><br>
+
+        <label for="vier">Wat heb je altijd mee als je op vakaniet gaat?</label>
+        <input type="text" id="vier" name="vier" required><br>
+
+        <label for="vijf">Wat is je beste persoonlijke eigenschap?</label>
+        <input type="text" id="vijf" name="vijf" required><br>
+
+        <label for="zes">Wat is je slechste persoonlijke eigenschap?</label>
+        <input type="text" id="zes" name="zes" required><br>
+
+        <label for="zeven">Wat is het ergste dat je kan overkomen?</label>
+        <input type="text" id="zeven" name="zeven" required><br>
+
+        <input type="submit" value="Versturen">
     </form>
 </body>
 </html>
