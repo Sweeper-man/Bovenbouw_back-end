a34271a360593b2c423001bc790cf8b32d6561c7
Seantheman01 <sean.v.g@outlook.com>
2024-09-04 16:11:15 +0200

    story added

--- Forms/Mad_libs/onkunde.php
+++ Forms/Mad_libs/onkunde.php
@@ -15,15 +15,11 @@
         <a href="onkunde.php">Onkunde</a>
     </div>
     <form>
-        <?php
-            echo $_GET["één"];
-            echo $_GET["twee"];
-            echo $_GET["drie"];
-            echo $_GET["vier"];
-            echo $_GET["vijf"];
-            echo $_GET["zes"];
-            echo $_GET["zeven"];
-        ?>
+        Er zijn veel mensen die niet kunnen <?php echo $_GET["één"]; ?>. Neem nou <?php echo $_GET["twee"]; ?>. 
+        Zelfs met de hulp van een <?php echo $_GET["vier"]; ?> of zelfs <?php echo $_GET["drie"]; ?> kan <?php echo $_GET["één"]; ?> niet <? echo $_GET["één"]; ?>.
+        Dat heeft niet te maken met een gebrek aan <?php echo $_GET["vijf"]; ?>, maar met een te veel aan <?php echo $_GET["zes"]; ?>.
+        Te veel <?php echo $_GET["zes"]; ?> leidt tot <?php echo $_GET["zeven"]; ?> en dat is niet goed als je wilt <?php echo $_GET["één"];?>.
+        Helaas voor <?php echo $_GET["twee"]; ?>.
     </form>
     <footer>
         © Sean van Gooswilligen
