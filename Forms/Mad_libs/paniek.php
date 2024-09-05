c3a350dfa502845e16ccabd327ce0674051524f9
Seantheman01 <sean.v.g@outlook.com>
2024-09-05 13:58:41 +0200

    fixed last errors

--- Forms/Mad_libs/paniek.php
+++ Forms/Mad_libs/paniek.php
@@ -17,22 +17,22 @@
     <form>
         Er heerst paniek in het koninkrijk <?php echo $_GET["drie"]; ?>. 
         Koning <?php echo $_GET["zes"]; ?> is ten eide raad en als koning <?php echo $_GET["zes"]; ?> ten eide raad is, 
-        dan roept hij zijn ten-eide-raadsheer <?php echo $_GET["twee"]; ?>.
+        dan roept hij zijn ten-eide-raadsheer <?php echo $_GET["twee"]; ?>.<br>
 
-        "<?php echo $_GET["twee"]; ?>! Het is een ramp. Het is een schande!"
+        "<?php echo $_GET["twee"]; ?>! Het is een ramp. Het is een schande!"<br>
 
-        "Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"
+        "Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"<br>
 
         "Mijn <?php echo $_GET["één"]; ?> is verdwenen! Zo maar, zonder waarschuwing. 
-        En ik had net <?php echo $_GET["vijf"]; ?> voor hem gekocht!"
+        En ik had net <?php echo $_GET["vijf"]; ?> voor hem gekocht!"<br>
 
-        "Majesteit, uw <?php echo $_GET["één"]; ?>" komt vast vanzelf weer terug?"
+        "Majesteit, uw <?php echo $_GET["één"]; ?> komt vast vanzelf weer terug?"<br>
 
-        "Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_GET["acht"]; ?> leren?"
+        "Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_GET["acht"]; ?> leren?"<br>
 
-        "Maar Sire, daar kunt u toch uw <?php echo $_GET["zeven"]; ?> voor gebruiken."
+        "Maar Sire, daar kunt u toch uw <?php echo $_GET["zeven"]; ?> voor gebruiken."<br>
 
-        "<?php echo $_GET["twee"]; ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."
+        "<?php echo $_GET["twee"]; ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."<br>
 
         "<?php echo $_GET["vier"]; ?>, Sire."
     </form>
